95d399027ea4aad5655c99e20453c2bd91ba8f28
Hugo Raymond <hugo.raymond@gmail.com>
2019-03-30 18:37:54 +0000

    bullet runs the full length of the screen but doesn't stop once it's reached the height of it. It also currently replaces the player on the screen

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const squares = []
   let aliens = [18,19,20,21,22,23,24,25,26,27,28,29,34,35,36,37,38,39,40,41,42,43,44,45]
   let playerIndex = 247
-  // const bullet = 247
+  let bulletIndex = playerIndex
 
   // ====================== FUNCTIONS =====================
   // function playerPosition(){
@@ -18,13 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
   function movePlayer() {
     // find the square with the class of "player"
     const player = squares.find(square => square.classList.contains('player'))
-    console.log(player, 'Added Player')
     // remove the class of player from that square
     player.classList.remove('player')
-    console.log(player, 'Removed player')
     // add the class of player to square the player should move to
     addPlayerClass()
-    console.log(player, 'Added again')
   }
 
   // ======================== GRID ========================
@@ -34,23 +31,21 @@ document.addEventListener('DOMContentLoaded', () => {
     grid.appendChild(square)
   }
 
-
   // ======================= PLAYER =======================
   // Set the player inside the grid
   addPlayerClass()
-
   // Setting up keycodes to enable player movement
   document.addEventListener('keydown', (e) => {
     switch(e.keyCode) {
       case 37:
-        // Move Left --> Left Key
+      // Move Left --> Left Key
         if(playerIndex % width > 0) {
           playerIndex--
           movePlayer()
         }
         break
       case 39:
-        // Move Right --> Right Key
+      // Move Right --> Right Key
         if(playerIndex % width < width - 1) {
           playerIndex++
           movePlayer()
@@ -60,27 +55,42 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   // ======================= LASERS =======================
-  // Find the position of the player – make sure this is in the global scope
-  // Add a class of bullet to the 
+  // Find the position of the player by using the playerIndex
+
+  // Add an eventListener with Keydown for the spacebar that will generate the bullet
+  // Add event listener on space bar to fire bullet ----------------------
+  document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 32) {
+      setInterval(() => {
+        squares[bulletIndex].classList.add('bullet')
+        bulletIndex -= width
+        squares[bulletIndex].classList.remove('bullet')
+      }, 80)
+    }
+  })
+  //Add the bullet to the div ABOVE the player's current position (this should be - width of the current position)
+  // Create a setInterval function for the bullet which will then -width * width (as the height is the same in my case) so that the bullet goes from it's start position to the end at the top of the container
+
+
+
 
   // ======================= ALIENS =======================
   // Create the aliens so they appear as a child of the divs that have been created by creating additional classes for .alien
-  // aliens.forEach((alienIndex) => {
-  //   squares[alienIndex].classList.add('alien')
-  // })
+  aliens.forEach((alienIndex) => {
+    squares[alienIndex].classList.add('alien')
+  })
 
   // Alien movement to remove class and then add class again using a set interval
-  // setInterval(() => {
-  //   aliens.forEach((alienIndex) => {
-  //     squares[alienIndex].classList.remove('alien')
-  //   })
-  //
-  //   aliens = aliens.map(alienIndex => alienIndex +1)
-  //   console.log(aliens)
-  //   aliens.forEach((alienIndex) => {
-  //     squares[alienIndex].classList.add('alien')
-  //   })
-  // }, 500)
+  setInterval(() => {
+    aliens.forEach((alienIndex) => {
+      squares[alienIndex].classList.remove('alien')
+    })
+
+    aliens = aliens.map(alienIndex => alienIndex +1)
+    aliens.forEach((alienIndex) => {
+      squares[alienIndex].classList.add('alien')
+    })
+  }, 500)
 
   // Restrict the width of their movement so that they can only run across 12 central divs
 
