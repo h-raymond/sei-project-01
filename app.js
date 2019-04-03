6db25bc76040028f080a8fd7957dd3ac39e7a7ce
Hugo Raymond <hugo.raymond@gmail.com>
2019-04-03 17:05:19 +0100

    Reset is now resetting the aliens in the right place

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// ====================== VARIABLES NOT REQUIRED BEFORE DOM LOADS =====================
+
+const width = 16
+const squares = []
+const aliensStart = [2,3,4,5,6,7,8,9,10,11,12,13,18,19,20,21,22,23,24,25,26,27,28,29,34,35,36,37,38,39,40,41,42,43,44,45]
+const alienMovement = [1,1,width,-1,-1,-1,-1,width,1,1]
+let aliens = aliensStart.slice()
+let playerIndex = 247
+let alienMove = 0
+let scoreTotal = 0
+let bombInterval = 0
+let livesRemaining = 1
+
 document.addEventListener('DOMContentLoaded', () => {
 
   // ====================== VARIABLES =====================
@@ -9,18 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const playerLives = document.querySelectorAll('.lives img')
   const endGame = document.querySelector('.end-game')
   const playAgainBtn = document.querySelector('#play-again')
-  const width = 16
-  const squares = []
-  const aliensStart = [2,3,4,5,6,7,8,9,10,11,12,13,18,19,20,21,22,23,24,25,26,27,28,29,34,35,36,37,38,39,40,41,42,43,44,45]
-  const alienMovement = [1,1,width,-1,-1,-1,-1,width,1,1]
-  let aliens = aliensStart
-  let playerIndex = 247
-  let alienMove = 0
-  let scoreTotal = 0
-  let bombInterval = 0
-  let livesRemaining = 3
 
-  // ====================== FUNCTIONS =====================
 
   function addPlayerClass(){
     squares[playerIndex].classList.add('player')
@@ -33,77 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // add the class of player to square the player should move to
     addPlayerClass()
   }
-  // startGame function
-
-  // clear all the alien classes from the grid
-  // re-set the alien array
-  // re-set the score
-  // apply the alien classes
-  // start the movement
-  // start the bombs dropping
-  // hide the start/end screen
-
-  function newGame() {
-    endGame.classList.add('hidden')
-    playerInfo.classList.remove('hidden')
-    playerIndex = 247
-    livesRemaining = 3
-    scoreTotal = 0
-    bombInterval = 0
-    alienMove = 0
-    aliensStart
-    clearInterval(bombInterval)
-    clearInterval(alienTimer)
-    clearInterval(alienBombTimer)
-    moveAliens()
-    alienBomb()
-  }
-  function gameOver() {
-    clearInterval(bombInterval)
-    clearInterval(alienTimer)
-    clearInterval(alienBombTimer)
-    // Display end game screen
-    grid.style.display = 'none'
-    endGame.classList.remove('hidden')
-    playerInfo.classList.add('hidden')
-    finalScore.innerText = scoreTotal
-  }
-
-  // ======================== GRID ========================
-
-  for(let i = 0; i < width ** 2; i++) {
-    const square = document.createElement('div')
-    squares.push(square)
-    grid.appendChild(square)
-  }
-
-  // ======================= PLAYER =======================
-
-  // Set the player inside the grid
-  addPlayerClass()
-  // Setting up keycodes to enable player movement
-  document.addEventListener('keydown', (e) => {
-    switch(e.keyCode) {
-      case 37:
-      // Move Left --> Left Key
-        if(playerIndex % width > 0) {
-          playerIndex--
-          movePlayer()
-        }
-        break
-      case 39:
-      // Move Right --> Right Key
-        if(playerIndex % width < width - 1) {
-          playerIndex++
-          movePlayer()
-        }
-        break
-    }
-  })
-
   function fireMissile(){
     let missileIndex = playerIndex - width
     let missileTarget = squares[missileIndex]
+
     const missileInterval = setInterval(() => {
       missileTarget.classList.remove('missile')
 
@@ -133,7 +68,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }, 80)
   }
-
   function moveAliens() {
     aliens.forEach((alienIndex) => {
       squares[alienIndex].classList.remove('alien')
@@ -153,7 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
       gameOver()
     }
   }
-
   function alienBomb() {
     // const randomIndex = Math.floor(Math.random() * 10)
     let bombIndex = aliens[Math.floor(Math.random() * aliens.length)]
@@ -179,45 +112,91 @@ document.addEventListener('DOMContentLoaded', () => {
           }
           // End the game, ie stop all the intervals
           if(livesRemaining === 0){ //OR ALIENS IN ARRAY REACH THE BOTTOM
-            clearInterval(bombInterval)
-            clearInterval(alienTimer)
-            clearInterval(alienBombTimer)
-            // Display end game screen
-            grid.style.display = 'none'
-            endGame.classList.remove('hidden')
-            playerInfo.classList.add('hidden')
-            finalScore.innerText = scoreTotal
+            gameOver()
           }
           squares[bombIndex].classList.remove('bomb')
         }
-      // otherwise just remove the bomb because we'll go out of index
+        // otherwise just remove the bomb because we'll go out of index
       } else {
         squares[bombIndex].classList.remove('bomb')
       }
     }, 500)
   }
+  function newGame() {
+    // Elements that have to be reset to start a newGame
+    endGame.classList.add('hidden')
+    playerInfo.classList.remove('hidden')
+    grid.style.display = 'flex'
+    scoreTotal = 0
+    score.innerText = scoreTotal
+    aliens = aliensStart
+    //Functions to run to allow the game to begin
+    addPlayerClass()
+    moveAliens()
+    alienBomb()
+  }
+  function gameOver() {
+    clearInterval(bombInterval)
+    clearInterval(alienTimer)
+    clearInterval(alienBombTimer)
+    // Display end game screen
+    grid.style.display = 'none'
+    endGame.classList.remove('hidden')
+    playerInfo.classList.add('hidden')
+    finalScore.innerText = score.innerText
+    //Reset all the other elements of the game play to restart on newGame() being triggered
+    playerLives.forEach(life => {
+      life.classList.remove('hidden')
+    })
+    livesRemaining = 3
+    playerIndex = 247
+    bombInterval = 0
+    alienMove = 0
+    aliens.forEach((alienIndex) => {
+      console.log('clearing', alienIndex)
+      squares[alienIndex].classList.remove('alien')
+    })
+  }
 
-  // ======================= MISSILE =======================
-  // Find the position of the player by using the playerIndex
-  // Add an eventListener with Keydown for the spacebar that will generate the missile
+  const alienTimer = setInterval(moveAliens, 500)
+  const alienBombTimer = setInterval(alienBomb, 1000)
+
+  for(let i = 0; i < width ** 2; i++) {
+    const square = document.createElement('div')
+    squares.push(square)
+    grid.appendChild(square)
+  }
+
+  addPlayerClass()
+  document.addEventListener('keydown', (e) => {
+    switch(e.keyCode) {
+      case 37:
+      // Move Left --> Left Key
+        if(playerIndex % width > 0) {
+          playerIndex--
+          movePlayer()
+        }
+        break
+      case 39:
+      // Move Right --> Right Key
+        if(playerIndex % width < width - 1) {
+          playerIndex++
+          movePlayer()
+        }
+        break
+    }
+  })
+
   document.addEventListener('keydown', (e) => {
     if(e.keyCode === 32) fireMissile()
   })
 
   playAgainBtn.addEventListener('click', newGame)
 
-
-  // ======================= ALIENS =======================
-  // Create the aliens so they appear as a child of the divs that have been created by creating additional classes for .alien
   aliens.forEach((alienIndex) => {
     squares[alienIndex].classList.add('alien')
   })
 
-  // Alien movement to remove class and then add class again using a set interval
-  const alienTimer = setInterval(moveAliens, 500)
-
-  // ===================== ALIEN BOMBS =====================
-  const alienBombTimer = setInterval(alienBomb, 1000)
 
   // CLOSING OF DOMContentLoaded
 })
